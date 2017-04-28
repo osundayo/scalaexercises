be7746434bf618089822682712dae8b052f6e155
osundayo <da.sun@exchange.co.jp>
2017-04-28 18:15:27 +0900

    FPinScala Exercise2.2

---
+++ Exercise2.2.scala
@@ -0,0 +1,8 @@
+def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+	def loop(n: Int): Boolean = {
+		if(n >= as.length - 1) true
+		else if(ordered(as(n), as(n+1))) loop(n+1)
+		else false
+	}
+	loop(0)
+}
